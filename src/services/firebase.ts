96eedb640046521d36b60f1774815b1809a7ea7d
Noah Dorfman <noahdorfman@macbookair.mynetworksettings.com>
2025-05-16 17:58:56 -0400

    moved firebase secrets

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,14 +4,14 @@ import { getDatabase } from 'firebase/database';
 import { getFunctions, connectFunctionsEmulator } from 'firebase/functions';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCuBgax1GXPP81-YKzFCBy_PFLo9u1y5ys",
-    authDomain: "flashcards-d25b9.firebaseapp.com",
-    databaseURL: "https://flashcards-d25b9-default-rtdb.firebaseio.com",
-    projectId: "flashcards-d25b9",
-    storageBucket: "flashcards-d25b9.firebasestorage.app",
-    messagingSenderId: "43665516647",
-    appId: "1:43665516647:web:db4a0457279ebfabc90cc2",
-    measurementId: "G-37CZVP4786"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
